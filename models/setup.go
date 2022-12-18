657d9cb687d0bcaee39c96fba2ec377c92daedf7
Surendra Made <zuren.dra96@gmail.com>
2022-12-18 17:22:31 +0800

    update

--- models/setup.go
+++ models/setup.go
@@ -2,7 +2,10 @@ package models
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,10 +13,22 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:8889)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "localhost", "golang")
+	err := godotenv.Load()
+	if err != nil {
+		panic("Gagal melakukan load env")
+	}
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	DbUser := os.Getenv("DB_USER")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DBHost := os.Getenv("DB_HOST")
+	DBName := os.Getenv("DB_NAME")
+	DBPort := os.Getenv("DB_PORT")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:"+DBPort+")/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DBHost, DBName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("Failed to create a connection to database")
+		panic("Failed to create a connection to database 111" + DbUser)
 	}
 	db.AutoMigrate(&User{}, &Product{})
 	DB = db
